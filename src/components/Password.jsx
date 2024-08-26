b8b9546ff6a863a7f59fb0adf8bf66616767429b
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-26 09:07:55 +0300

    add button

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -2,6 +2,11 @@
 
 export default function Password() {
   return (
-    <div>Password</div>
+      <div className="container">
+          <h3>Password Generator</h3>
+          <div className="pass__containers">
+              <button id="clipboad" className="btn__display"><i></i></button>
+          </div>
+    </div>
   )
 }
