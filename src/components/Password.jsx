5525400ff522092e332cc6bba3f1318abb2aa780
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-26 09:16:23 +0300

    add clipboard icon

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,12 +1,15 @@
-
+import { FaClipboard } from "react-icons/fa";
 
 export default function Password() {
   return (
-      <div className="container">
-          <h3>Password Generator</h3>
-          <div className="pass__containers">
-              <button id="clipboad" className="btn__display"><i></i></button>
-          </div>
+    <div className="container">
+      <h3>Password Generator</h3>
+          <div className="pass__container">
+              <span></span>
+        <button id="clipboad" className="btn__display">
+          <FaClipboard />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
