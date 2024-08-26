20ef86ce03302d970fec2cd4b2d4486e0d60e664
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-27 00:01:13 +0300

    add char file

---
+++ src/components/CharSet.js
@@ -0,0 +1,4 @@
+export const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+export const numberChars = '0123456789';
+export const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
