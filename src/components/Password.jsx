ea55bf6402f477dc254fd21935c73eb15615a323
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-26 11:09:22 +0300

    add bootstrap switch input

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -4,34 +4,48 @@ export default function Password() {
   return (
     <div className="container">
       <h3>Password Generator</h3>
-          <div className="pass__container">
-              <span></span>
+      <div className="pass__container">
+        <span></span>
         <button id="clipboad" className="btn__display">
           <FaClipboard />
         </button>
-          </div>
-          <div className="settings">
-              <div className="setting">
-                  <label htmlFor="">Password Length</label>
-                  <input type="range" className="form-range" min="6" max="24" step="6" />
-              </div>
-              <div className="setting">
-                  <label htmlFor="">Include Uppercase Letters</label>
-                  <input type="checkbox" />
-              </div>
-              <div className="setting">
-                  <label htmlFor="">Include Lowercase Letters</label>
-                  <input type="checkbox" />
-              </div>
-              <div className="setting">
-                  <label htmlFor="">Include Numbers</label>
-                  <input type="checkbox" />
-              </div>
-              <div className="setting">
-                  <label htmlFor="">Include Symbols</label>
-                  <input type="checkbox" />
-              </div>
-          </div>
+      </div>
+      <div className="settings">
+        <div className="setting">
+          <label htmlFor="">Password Length</label>
+          <input
+            type="range"
+            className="form-range"
+            min="6"
+            max="24"
+            step="6"
+          />
+        </div>
+        <div className="setting form-check form-switch">
+          <label className="form-check-label" htmlFor="">
+            Include Uppercase Letters
+          </label>
+          <input className="form-check-input" type="checkbox" />
+        </div>
+        <div className="setting form-check form-switch">
+          <label className="form-check-label" htmlFor="">
+            Include Lowercase Letters
+          </label>
+          <input className="form-check-input" type="checkbox" />
+        </div>
+        <div className="setting form-check form-switch">
+          <label className="form-check-label" htmlFor="">
+            Include Numbers
+          </label>
+          <input className="form-check-input" type="checkbox" />
+        </div>
+        <div className="setting form-check form-switch">
+          <label className="form-check-label" htmlFor="">
+            Include Symbols
+          </label>
+          <input className="form-check-input" type="checkbox" />
+        </div>
+      </div>
     </div>
   );
 }
